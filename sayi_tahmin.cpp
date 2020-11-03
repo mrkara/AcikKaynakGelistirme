1a088e19761062997367a95b2ef0c887620da48a
buseorak <73755546+buseorak@users.noreply.github.com>
2020-11-03 17:14:36 +0300

    corrected and added some lines

--- sayi_tahmin.cpp
+++ sayi_tahmin.cpp
@@ -5,16 +5,27 @@ int main()
 {
     int tut = 5;
     int tahmin;
-    std::string bilgi = "Bu program kullanıcının bir sayı girmesini bekler ve programın tuttuğu sayıya eşit olup olmadığını kontrol eder.";
+    
+    std::string bilgi = "Bu program kullanıcının bir sayı girmesini bekler ve "
+    "bu sayının, programın tuttuğu sayıya eşit olup olmadığını kontrol eder.";
+    
+    std::cout << bilgi << std::endl;
     std::cout << "0 ve 10 arasında bir tam sayı giriniz." << std::endl;
     std::cin >> tahmin;
 
-    if (tahmin == tut) {
-        std::cout << "Tebrikler!" << std::endl;
+    if (tahmin > 10) {
+        std::cout << "Lütfen 10'dan büyük sayı girmeyiniz." << std::endl;
+    }
+    else if (tahmin < 0) {
+        std::cout << "Lütfen negatif sayılar girmeyiniz." << std::endl;
     }
     else {
-        std::cout << "Tekrar deneyin!" << std::endl;
+        if (tahmin == tut) {
+            std::cout << "Tebrikler!" << std::endl;
+        }
+        else {
+            std::cout << "Tekrar deneyin!" << std::endl;
+        }
     }
-
-    return 0;
+        return 0;
 }
