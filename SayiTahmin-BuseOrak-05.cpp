0c2381e92a752bf25b094c7f1011eff8e1aaa4f4
Muhammet Kara <muhammet.kara@collabora.com>
2020-11-06 00:33:06 +0300

    Fix styling with clang-format
    Fixed by: clang-format -i SayiTahmin-BuseOrak-05.cpp

--- SayiTahmin-BuseOrak-05.cpp
+++ SayiTahmin-BuseOrak-05.cpp
@@ -5,28 +5,34 @@ int main()
 {
     const int tut = 5;
     int tahmin;
-    
-    const std::string bilgi = "Bu program kullanıcının bir sayı girmesini bekler ve "
-    "bu sayının, programın tuttuğu sayıya eşit olup olmadığını kontrol eder.";
-    
+
+    const std::string bilgi
+        = "Bu program kullanıcının bir sayı girmesini bekler ve "
+          "bu sayının, programın tuttuğu sayıya eşit olup olmadığını kontrol eder.";
+
     std::cout << bilgi << std::endl;
     std::cout << "0 ve 10 arasında bir tam sayı giriniz." << std::endl;
     std::cin >> tahmin;
 
-    if (tahmin > 10) {
+    if (tahmin > 10)
+    {
         std::cout << "Lütfen 10'dan büyük sayı girmeyiniz." << std::endl;
     }
-    else if (tahmin < 0) {
+    else if (tahmin < 0)
+    {
         std::cout << "Lütfen negatif sayılar girmeyiniz." << std::endl;
     }
-    else {
-        if (tahmin == tut) {
+    else
+    {
+        if (tahmin == tut)
+        {
             std::cout << "Tebrikler!" << std::endl;
         }
-        else {
+        else
+        {
             std::cout << "Tekrar deneyin!" << std::endl;
         }
     }
-    
+
     return 0;
 }
