cb27ba1946daaa543f4f241696900a7fa169f4f7
Muhammet Kara <muhammet.kara@collabora.com>
2020-11-06 23:50:57 +0300

    Fix styling with clang-format

--- Hafta01/Cozumler/SayiTahmin-MücahidAydin-17.cpp
+++ Hafta01/Cozumler/SayiTahmin-MücahidAydin-17.cpp
@@ -4,35 +4,30 @@
 
 int main()
 {
-  
-
-    int x,a;
+    int x, a;
 
     srand(time(0));
-    x=rand() %100;
-   
-  
+    x = rand() % 100;
 
-while (true) {
-    std::cout << "0-99 arasında bir değer girin." << std::endl;
-    std::cin >> a;
+    while (true)
+    {
+        std::cout << "0-99 arasında bir değer girin." << std::endl;
+        std::cin >> a;
 
-   // std::cout << x << std::endl;
-    if(a==x)
-    {
-        std::cout << "Tebrikler" << std::endl;
-        break;
+        // std::cout << x << std::endl;
+        if (a == x)
+        {
+            std::cout << "Tebrikler" << std::endl;
+            break;
+        }
+        else if (a <= x)
+        {
+            std::cout << "Daha büyük bir sayı girin" << std::endl;
+        }
+        else if (a >= x)
+        {
+            std::cout << "Daha küçük bir sayı girin" << std::endl;
+        }
     }
-    else if(a<=x)
-    {
-        std::cout << "Daha büyük bir sayı girin" << std::endl;
-    }
-    else if (a>=x) {
-        std::cout << "Daha küçük bir sayı girin" << std::endl;
-    }
-   
-    
-}
     return 0;
-
 }
