e437f49c302e3681bfc23466bc0e1b7ed91677e8
Muhammet Kara <muhammet.kara@collabora.com>
2020-11-06 23:58:41 +0300

    Fix styling with clang-format based on repo rules

--- Hafta01/Cozumler/SayiTahmin-BayramCicek-01.cpp
+++ Hafta01/Cozumler/SayiTahmin-BayramCicek-01.cpp
@@ -8,21 +8,22 @@
 #include <iostream>
 #include <string>
 
-void compare(int tahmin, int tut) {
+void compare(int tahmin, int tut)
+{
     if (tahmin == tut)
         std::cout << "Tebrikler!" << std::endl;
     else
         std::cout << "Tekrar deneyin!" << std::endl;
 }
 
-int main(int argc, char **argv) {
-
+int main(int argc, char** argv)
+{
     // sayı tut
     const int tut = 7;
     int tahmin;
     const std::string message = "Bu program [1, 10] aralığında bir sayı tutar.\n"
-                          "Girilen sayının programın tuttuğu sayıya\n"
-                          "eşit olup olmadığını kontrol eder.\n";
+                                "Girilen sayının programın tuttuğu sayıya\n"
+                                "eşit olup olmadığını kontrol eder.\n";
 
     // bilgi ver
     std::cout << message << std::endl;
