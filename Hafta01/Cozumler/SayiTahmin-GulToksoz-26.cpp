76ee2e8016096ea64876e12b7d6d594951d094a5
Gultoksoz <meryem.toksoz@ogr.sakarya.edu.tr>
2020-11-11 11:22:46 +0000

    fiz code styling

--- Hafta01/Cozumler/SayiTahmin-GulToksoz-26.cpp
+++ Hafta01/Cozumler/SayiTahmin-GulToksoz-26.cpp
@@ -3,24 +3,25 @@
 
 int main()
 {
-	std::cout << "Bu program 1 ile 10 arasinda tutulan rastgele sayinin tahmin edilmesini sağlar " << std :: endl;
+    std::cout << "Bu program 1 ile 10 arasinda tutulan rastgele sayinin tahmin edilmesini sağlar "
+              << std ::endl;
 
     srand(time(0));
 
-	int rastgeleSayi = rand() % 11;
-	int tahmin;
-    
-	std :: cout << "1 ile 10 arasinda bir tam sayi tahmin ediniz..: ";
-	std :: cin >> tahmin;
-	
-	if (tahmin == rastgeleSayi)
-	{
-		std :: cout << std :: endl << "Tebrikler !";
-	}
-	else
-	{
-		std :: cout << std :: endl << "Tekrar deneyin !";
-	}
-    
-	return 0;
+    int rastgeleSayi = rand() % 11;
+    int tahmin;
+
+    std ::cout << "1 ile 10 arasinda bir tam sayi tahmin ediniz..: ";
+    std ::cin >> tahmin;
+
+    if (tahmin == rastgeleSayi)
+    {
+        std ::cout << std ::endl << "Tebrikler !";
+    }
+    else
+    {
+        std ::cout << std ::endl << "Tekrar deneyin !";
+    }
+
+    return 0;
 }
