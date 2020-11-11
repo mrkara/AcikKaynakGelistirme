bff6cb1536ac54f64bc962a015c196a568d9bd6f
Gultoksoz <meryem.toksoz@ogr.sakarya.edu.tr>
2020-11-11 10:52:42 +0000

    sayiTahmin

---
+++ Hafta01/Cozumler/SayiTahmin-GulToksoz-26.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <ctime>
+
+int main()
+{
+	std::cout << "Bu program 1 ile 10 arasinda tutulan rastgele sayinin tahmin edilmesini sağlar " << std :: endl;
+
+    srand(time(0));
+
+	int rastgeleSayi = rand() % 11;
+	int tahmin;
+    
+	std :: cout << "1 ile 10 arasinda bir tam sayi tahmin ediniz..: ";
+	std :: cin >> tahmin;
+	
+	if (tahmin == rastgeleSayi)
+	{
+		std :: cout << std :: endl << "Tebrikler !";
+	}
+	else
+	{
+		std :: cout << std :: endl << "Tekrar deneyin !";
+	}
+    
+	return 0;
+}
