7034b8bad933b53be297f41dc0b58e3ab6cb41c9
bedrag <bedra5.bgk@gmail.com>
2020-11-14 07:57:12 +0000

    ilk hafta odevi

---
+++ SayiTahmin-BedraGoksu-02.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+
+std::string info = "Bu program kullanicidan 1 ile 10 arasi bir tam sayi girmesini ister ve kullanicin tahmininin dogrulugunu test eder.";
+
+int main()
+{
+    const int tut = 7;
+    int tahmin;
+    
+    std::cout << info << std::endl;
+    std::cout << "1 ile 10 arasi tam sayi giriniz: ";
+    std::cin >> tahmin;
+
+    if ( tut == tahmin )
+    {
+        std::cout << "Tebrikler tahmininiz dogru!" << std::endl;
+    }
+
+    else 
+    {
+        std::cout << "Tekrar deneyiniz." << std::endl;
+    }
+    
+    return 0;
+}
