6474de454abf0d15596584be306197aec8fc812b
celikmustaa <mustafaclk253@gmail.com>
2020-12-10 12:02:38 +0000

    fix code-styling with lang-format

--- Hafta01/Cozumler/SayiTahmin-MustafaCelik-21986176.cpp
+++ Hafta01/Cozumler/SayiTahmin-MustafaCelik-21986176.cpp
@@ -1,18 +1,20 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-    int a=8;
+int main()
+{
+    int a = 8;
     cout << "Bir sayı giriniz:\n";
     int b;
-    cin>>b;
-    if (a==b){
-        cout<<"Tebrikler, sayılar aynı!"<<endl;
+    cin >> b;
+    if (a == b)
+    {
+        cout << "Tebrikler, sayılar aynı!" << endl;
     }
-    else{
-        cout<<"Sayılar aynı değil :/ "<<endl;
+    else
+    {
+        cout << "Sayılar aynı değil :/ " << endl;
     }
-    
 
     return 0;
 }
